f924cc4642d30270589e846936b566d621448306
Sideboard <sideboard@revolutionarts.de>
2024-12-06 20:52:54 +0100

    Add day3::part2

--- src/day3.rs
+++ src/day3.rs
@@ -8,3 +8,20 @@ pub fn part1(input: &str) -> i32 {
     }).sum();
     return sum;
 }
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> i32 {
+    let re = Regex::new(r"(?:(?:mul\((\d{1,3}),(\d{1,3})\))|(?:do(?:n't)?))").unwrap();
+    let mut active = true;
+    let mut sum = 0;
+    for cap in re.captures_iter(input) {
+        if &cap[0] == "do" || &cap[0] == "don't" {
+            active = &cap[0] == "do";
+            continue;
+        }
+        if active {
+            sum += cap[1].parse::<i32>().unwrap() * cap[2].parse::<i32>().unwrap();
+        }
+   }
+    return sum;
+}
