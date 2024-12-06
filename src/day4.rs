d3ac9c0662736d41c0c7ea531b36edd0bc2f655e
Sideboard <sideboard@revolutionarts.de>
2024-12-06 22:41:24 +0100

    Add day4::part2

--- src/day4.rs
+++ src/day4.rs
@@ -40,3 +40,44 @@ pub fn part1(input: &str) -> i32 {
 
     return count;
 }
+
+#[aoc(day4, part2)]
+pub fn part2(input: &str) -> i32 {
+    let n_lines = input.lines().count();
+    let line_len_list = input.lines().map(|line| line.chars().count()).collect::<Vec<_>>();
+    let n_chars = line_len_list[0];
+    let is_same_len = line_len_list.iter().all(|&len| len == n_chars);
+    assert!(is_same_len, "All lines must have the same length");
+
+    let mut matrix = vec![vec![' '; n_chars]; n_lines];
+    for (i, line) in input.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            matrix[i][j] = c;
+        }
+    }
+
+    let mut count = 0;
+    for (i, line) in matrix.iter().enumerate() {
+        for (j, c) in line.iter().enumerate() {
+            if c != &'A' {
+                continue;
+            }
+            if i == 0 || j == 0 || i == n_lines - 1 || j == n_chars - 1 {
+                continue;
+            }
+            let ip = i + 1;
+            let jp = j + 1;
+            let im = i - 1;
+            let jm = j - 1;
+            if (matrix[im][jm] == 'M' && matrix[ip][jp] == 'S'  ||
+                matrix[im][jm] == 'S' && matrix[ip][jp] == 'M') &&
+               (matrix[im][jp] == 'M' && matrix[ip][jm] == 'S' ||
+                matrix[im][jp] == 'S' && matrix[ip][jm] == 'M') {
+                println!("{}{}{}", matrix[im][jp], matrix[i][j], matrix[ip][jm]);
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
